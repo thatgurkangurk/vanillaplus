d748de6b9d05ae134fce8d5e93442c5cf7c552dc
Gurkan <hello@gurkz.me>
2025-04-21 13:54:56 +0200

    use datagen (#2)
    * use datagen
    
    * bump version

--- src/client/java/me/gurkz/vanillaplus/client/VanillaPlusDataGenerator.java
+++ src/client/java/me/gurkz/vanillaplus/client/VanillaPlusDataGenerator.java
@@ -1,5 +1,9 @@
 package me.gurkz.vanillaplus.client;
 
+import me.gurkz.vanillaplus.client.datagen.ModBlockTagProvider;
+import me.gurkz.vanillaplus.client.datagen.ModLootTableProvider;
+import me.gurkz.vanillaplus.client.datagen.ModModelProvider;
+import me.gurkz.vanillaplus.client.datagen.ModRecipeProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
@@ -8,5 +12,10 @@ public class VanillaPlusDataGenerator implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+        pack.addProvider(ModBlockTagProvider::new);
+        pack.addProvider(ModLootTableProvider::new);
+        pack.addProvider(ModModelProvider::new);
+        pack.addProvider(ModRecipeProvider::new);
     }
 }
